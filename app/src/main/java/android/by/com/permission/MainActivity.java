19e529b362c77c414cc5cd00869a1ac5b914ee61
by.huang <331287556@qq.com>
2017-07-27 17:36:57 +0800

    New permission

---
+++ app/src/main/java/android/by/com/permission/MainActivity.java
@@ -0,0 +1,13 @@
+package android.by.com.permission;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
